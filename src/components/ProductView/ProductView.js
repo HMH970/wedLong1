b4e939c8df24a538c9097e795376e6fb29409068
Andrew Stilinovic <stili87@gmail.com>
2022-05-18 15:57:19 -0500

    halfway through phase 2

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -2,34 +2,42 @@ import React from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
+import { useState } from 'react';
 
 function ProductView({ products }) {
-
-    // TODO: Replace with state variable
-    const sideOpen = true;
+    const [sideOpen, setSideOpen] = useState(true);
+    const [item, setItem] = useState('')
+    // const [isSelected, setSelection] = useState(false)
 
     return (
         <div className="product-view">
             <div className="product-main-area">
                 <h1>Products</h1>
                 <div className="product-list">
-                    {products.map(item =>
-                        <ProductListItem
+                    {products.map(item => {
+                        let isSelected = false
+                        return <ProductListItem
                             key={item.id}
                             product={item}
-                            onClick={() => console.log('SELECT PRODUCT', item)}
+                            onClick={() => {
+                                isSelected = true
+                                return setItem(item)
+                            }}
+                            isSelected={isSelected}
+                            
+
                         />
-                    )}
+                    })}
                 </div>
             </div>
             <div className="product-side-panel">
                 <div className="product-side-panel-toggle-wrapper">
                     <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
+                         onClick={() => setSideOpen(!sideOpen)}>
                         {sideOpen ? '>' : '<'}
                     </div>
                 </div>
-                <ProductDetails visible={sideOpen} />
+                <ProductDetails product={item} visible={sideOpen} />
             </div>
         </div>
     );
